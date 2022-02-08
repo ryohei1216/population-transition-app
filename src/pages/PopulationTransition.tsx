eed629fcc1eadcbdea3716b61ae92600116c4e9b
ryohei1216 <ryohei123000000@gmail.com>
2022-02-08 11:53:17 +0900

    チェックリストの抽出

--- src/pages/PopulationTransition.tsx
+++ src/pages/PopulationTransition.tsx
@@ -1,16 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import '../css/PopulationTransition.css';
 import Prefectures from '../components/Prefectures';
-import usePrefecture from '../hooks/usePrefecture';
+import { Prefecture } from '../api/prefectures';
 
 const PopulationTransition: FC = () => {
+  const [prefectureList, setPrefectureList] = useState<Prefecture[]>([]);
+  console.log(prefectureList);
   return (
     <div className="container">
       <div>
         <h1>都道府県別の総人口推移グラフ</h1>
       </div>
       <div className="prefectures-container">
-        <Prefectures />
+        <Prefectures setPrefectureList={setPrefectureList} />
       </div>
     </div>
   );
